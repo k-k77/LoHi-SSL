aa85a5a7bc2a1ac8bd1e359d1b1066ac033c06ea
k-k77 <15621401971@163.com>
2025-12-28 19:35:45 +0800

    Update validation.py

--- LoHi-SSL/LoHi-SSL-main/validation.py
+++ LoHi-SSL/LoHi-SSL-main/validation.py
@@ -42,59 +42,6 @@ for key, value in label_dict.items():
 
 
 
-if not args.supervised:  # 执行无监督的细胞聚类验证
 
-    print('cell clustering validation')
-    Y_label = np.load('./datasets/example_dataset_simulation1/lbls.npy')
-    x_ach = np.load('./output/scMHNN_embedding.npy')
-    # print(Y_label.shape)
-    # print(x_ach.shape)
-
-    ASW_learned = []
-    PS_learned = []
-    HS_learned = []
-
-    embedding_name = []
-    cell_name = []
-    for i in range(x_ach.shape[1]):
-        embedding_name.append(str(i))
-
-    for i in range(x_ach.shape[0]):
-        cell_name.append(str(i))
-    embedding_name = pd.DataFrame(index=embedding_name)
-    cell_name = pd.DataFrame(index=cell_name)
-    adata_learned=ad.AnnData(x_ach,obs=cell_name,var=embedding_name)
-    Y_label_list = itemgetter(*list(Y_label))(label_dict_)
-    adata_learned.obs['cell_type'] = Y_label_list
-    sc.pp.neighbors(adata_learned,use_rep='X')
-    sc.tl.umap(adata_learned,n_components=2)
-    sc.pl.umap(adata_learned, color="cell_type")
-    plt.title('scMHNN')
-    plt.savefig('./output/scMHNN_umap_{}.jpg'.format(args.dataset), bbox_inches='tight', dpi=800)
-
-    for i in list(np.round(np.linspace(0.1,1.0,10),1)):
-        print('resolution:',i)
-
-        sc.tl.louvain(adata_learned,resolution = i,key_added = "louvain")  # best
-        y_predict = adata_learned.obs['louvain']
-
-        ASW_learned.append(np.round(silhouette_score(x_ach,y_predict),3))
-        PS_learned.append(np.round(purity_score(Y_label,y_predict),3))
-        HS_learned.append(np.round(homogeneity_score(Y_label,y_predict),3))
-
-    print('cell clustering results:')
-    print('ASW_learned = ',ASW_learned)
-    print('PS_learned = ',PS_learned)
-    print('HS_learned = ',HS_learned)
-
-else:
-    print('cell annotation validation')
-    df_result = pd.DataFrame()
-    lbls_test = np.load('./output/lbls_test.npy') 
-    preds_test = np.load('./output/preds_test.npy')
-
-    df_result['acc'] = [np.round(accuracy_score(lbls_test,preds_test),3)]
-    df_result['f1w'] = [np.round(f1_score(lbls_test,preds_test, average='weighted'),3)]
-    print(df_result)
 
 
